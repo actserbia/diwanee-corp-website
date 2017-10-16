33ce861b7117b30723a142348eb824a65ca35444
Katarina Caric <katarina.caric@diwanee.com>
2017-10-16 17:10:50 +0200

    removed double field

--- resources/views/admin/articles/articles_create.blade.php
+++ resources/views/admin/articles/articles_create.blade.php
@@ -76,16 +76,7 @@
                             </div>
                         </div>
                         
-                        <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="content">Content</label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <textarea id="content" name="content" class="form-control col-md-7 col-xs-12" name="content">{{ Request::old('content') ?: '' }}</textarea>
-                                @if ($errors->has('content'))
-                                    <span class="help-block">{{ $errors->first('content') }}</span>
-                                @endif
-                            </div>
-                        </div>
-                        
+
                         <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="image">Image</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
@@ -99,7 +90,7 @@
                         <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="content">Content</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                              <textarea id="content" name="content" class="js-st-instance sir-trevor editable">{{ Request::old('content') ?: '' }}</textarea>
+                              <textarea id="content" name="content" class="sir-trevor editable">{{ Request::old('content') ?: '' }}</textarea>
                                 @if ($errors->has('content'))
                                    <span class="help-block">{{ $errors->first('content') }}</span>
                                 @endif
--- resources/views/admin/articles/articles_edit.blade.php
+++ resources/views/admin/articles/articles_edit.blade.php
@@ -79,17 +79,7 @@
                                 @endif
                             </div>
                         </div>
-                            
-                        <div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="content">Content</label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <textarea id="content" name="content" class="form-control col-md-7 col-xs-12" name="content">{{ $article->content }}</textarea>
-                                @if ($errors->has('content'))
-                                    <span class="help-block">{{ $errors->first('content') }}</span>
-                                @endif
-                            </div>
-                        </div>
-                        
+
                         <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="image">Image</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
@@ -108,7 +98,7 @@
                           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="content">Content</label>
                           <div class="col-md-6 col-sm-6 col-xs-12">
 
-                            <textarea id="content" name="content" class="js-st-instance sir-trevor editable">{{ !$article->elements->isEmpty() ? $article->elements[0]->content : '' }}</textarea>
+                            <textarea id="content" name="content" class="sir-trevor editable">{{ $article->content }}</textarea>
                             @if ($errors->has('content'))
                             <span class="help-block">{{ $errors->first('content') }}</span>
                             @endif
