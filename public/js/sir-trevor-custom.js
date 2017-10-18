d47fc558f88807f27909992a47cf80d554ff4e2d
Katarina Caric <katarina.caric@diwanee.com>
2017-10-18 11:23:25 +0200

    custom elements sir trevor

--- public/js/sir-trevor-custom.js
+++ public/js/sir-trevor-custom.js
@@ -7,6 +7,12 @@ SirTrevor.Blocks.SliderImage = SirTrevor.Block.extend({
     uploadable: true,
 
     icon_name: 'iframe',        //'sliderimage',
+    textFields: {
+        seo_name: {sel: 'seoname', required: false},
+        seo_alt: {sel: 'seoalt', required: false},
+        caption: {sel: 'caption', required: false},
+        copyright: {sel: 'copyright', required: false}
+    },
 
     loadData: function(data){
         console.log(data.file.url);
@@ -19,6 +25,7 @@ SirTrevor.Blocks.SliderImage = SirTrevor.Block.extend({
         span.appendChild(t);
         this.editor.appendChild(image);
         this.editor.appendChild(span);
+        this.imageAddition(data);
     },
 
     onDrop: function(transferData){
@@ -39,6 +46,9 @@ SirTrevor.Blocks.SliderImage = SirTrevor.Block.extend({
             span.appendChild(t);
             this.editor.appendChild(span);
 
+            this.imageAddition();
+
+
             this.editor.style.display = '';
 
             this.uploader(
@@ -53,5 +63,114 @@ SirTrevor.Blocks.SliderImage = SirTrevor.Block.extend({
                 }
             );
         }
+    },
+
+    imageAddition: function(data){
+        console.log(data);
+        var that = this;
+        $.each(this.textFields, function(i, element) {
+            var div = document.createElement("div");
+            div.setAttribute('class', 'image_input_text');
+
+            var field = document.createElement("input");
+            field.setAttribute("type", "text");
+            field.setAttribute("name", element.sel);
+            field.setAttribute("class", element.sel);
+            //field.setAttribute("value", 'nesto');
+            var label = document.createElement("label");
+            var t = document.createTextNode(i);
+            label.setAttribute("for", i);
+            label.appendChild(t);
+            div.appendChild(label);
+            div.appendChild(field);
+
+            that.editor.appendChild(div);
+        });
+
     }
+
+});
+
+
+SirTrevor.Blocks.DiwaneeImage = SirTrevor.Block.extend({
+    type: "diwanee image",
+
+    droppable: true,
+    uploadable: true,
+
+    icon_name: 'image',
+    textFields: {
+        seo_name: {sel: 'seoname', required: false},
+        seo_alt: {sel: 'seoalt', required: false},
+        caption: {sel: 'caption', required: false},
+        copyright: {sel: 'copyrigh', required: false}
+    },
+
+    loadData: function(data){
+        console.log(data.file.url);
+        // Create our image tag
+        this.editor.innerHTML = '';
+        var image = document.createElement("img");
+        image.setAttribute("src", data.file.url);
+        this.editor.appendChild(image);
+        this.imageAddition(data);
+    },
+
+    onDrop: function(transferData){
+        var file = transferData.files[0],
+            urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
+
+        // Handle one upload at a time
+        if (/image/.test(file.type)) {
+            this.loading();
+            // Show this image on here
+            this.inputs.style.display = 'none';
+            this.editor.innerHTML = '';
+            var image = document.createElement("img");
+            image.setAttribute("src", urlAPI.createObjectURL(file));
+            this.editor.appendChild(image);
+
+            this.imageAddition();
+
+            this.editor.style.display = '';
+
+            this.uploader(
+                file,
+                function(data) {
+                    this.setData(data);
+                    this.ready();
+                },
+                function(error) {
+                    this.addMessage(i18n.t('blocks:image:upload_error'));
+                    this.ready();
+                }
+            );
+        }
+    },
+
+    imageAddition: function(data){
+        console.log(data);
+        var that = this;
+        $.each(this.textFields, function(i, element) {
+            var div = document.createElement("div");
+            div.setAttribute('class', 'image_input_text');
+
+            var field = document.createElement("input");
+            field.setAttribute("type", "text");
+            field.setAttribute("name", element.sel);
+            field.setAttribute("class", element.sel);
+            //field.setAttribute("value", '');
+            var label = document.createElement("label");
+            var t = document.createTextNode(i);
+            label.setAttribute("for", i);
+            label.appendChild(t);
+            div.appendChild(label);
+            div.appendChild(field);
+
+            that.editor.appendChild(div);
+        });
+
+    }
+
+
 });
--- public/js/sir-trevor.js
+++ public/js/sir-trevor.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
     SirTrevor.setDefaults({ uploadUrl: "/sir-trevor/images",  iconUrl: "/asset/sirtrevorjs/sir-trevor-icons.svg" });
 
     window.editor = new SirTrevor.Editor({
-        el: document.querySelector('.sir-trevor'),
+        el:document.querySelector('.sir-trevor'),
         defaultType: 'Text',
-        blockTypes: ['Text', 'List', 'Quote', 'Image', 'Video', 'Heading', 'SliderImage']
+        blockTypes: [ 'Text', 'List', 'Quote', 'DiwaneeImage', 'Video', 'Heading', 'SliderImage' ]
     });
+
 });
