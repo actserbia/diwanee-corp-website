2bafa4c3b83fd86f645fd28822286195f542b764
jelenaromcevic <jelena.romcevic@diwanee.com>
2017-10-20 02:24:05 +0200

    test

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
+
 mix.copy('node_modules/sir-trevor/build/*', 'public/asset/sirtrevorjs/');
 
 mix.autoload({
