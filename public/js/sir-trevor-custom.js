f7bcabe84b7d478255f5a998ef0bca39368cd881
Katarina Caric <katarina.caric@diwanee.com>
2017-10-23 10:59:25 +0200

    sir trevor collapse image data

--- public/js/sir-trevor-custom.js
+++ public/js/sir-trevor-custom.js
@@ -1,113 +1,5 @@
 "use strict";
 
-SirTrevor.Blocks.SliderImage = SirTrevor.Block.extend({
-    type: "slider image",
-
-    droppable: true,
-    uploadable: true,
-
-    icon_name: 'iframe',        //'sliderimage',
-    textFields: {
-        seo_name: {sel: 'seoname', required: false},
-        seo_alt: {sel: 'seoalt', required: false},
-        caption: {sel: 'caption', required: false},
-        copyright: {sel: 'copyright', required: false}
-    },
-
-    loadData: function(data){
-        console.log(data.file.url);
-        // Create our image tag
-        this.editor.innerHTML = '';
-        
-        var div = document.createElement("div");
-        var t = document.createTextNode("Slider Image");
-        div.appendChild(t);
-        this.editor.appendChild(div);
-        
-        var div = document.createElement("div");
-        var image = document.createElement("img");
-        image.setAttribute("src", data.file.url);
-        this.editor.appendChild(image);
-        
-        this.imageAddition(data);
-    },
-
-    onDrop: function(transferData){
-        var file = transferData.files[0],
-            urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
-
-        // Handle one upload at a time
-        if (/image/.test(file.type)) {
-            this.loading();
-            // Show this image on here
-            this.inputs.style.display = 'none';
-            this.editor.innerHTML = '';
-            
-            var div = document.createElement("div");
-            var t = document.createTextNode("Slider Image");
-            div.appendChild(t);
-            this.editor.appendChild(div);
-            
-            var image = document.createElement("img");
-            image.setAttribute("src", urlAPI.createObjectURL(file));
-            this.editor.appendChild(image);
-
-            this.imageAddition();
-
-
-            this.editor.style.display = '';
-
-            this.uploader(
-                file,
-                function(data) {
-                    this.setData(data);
-                    this.ready();
-                },
-                function(error) {
-                    this.addMessage(i18n.t('blocks:image:upload_error'));
-                    this.ready();
-                }
-            );
-        }
-    },
-
-    imageAddition: function(data){
-        console.log(data);
-        var that = this;
-        
-        var divAddition = document.createElement("div");
-        divAddition.setAttribute("class", "image-additional");
-        $.each(this.textFields, function(i, element) {
-            var div = document.createElement("div");
-            div.setAttribute('class', 'image_input_text');
-
-            var field = document.createElement("input");
-            field.setAttribute("type", "text");
-            field.setAttribute("name", element.sel);
-            field.setAttribute("class", element.sel);
-            if(data !== undefined) {
-              field.setAttribute("value", data[element.sel]);
-            }
-            if(element.required) {
-                field.setAttribute("required", true);
-            }
-            field.setAttribute("maxlength", 90);
-            var label = document.createElement("label");
-            var t = document.createTextNode(i);
-            label.setAttribute("for", i);
-            label.appendChild(t);
-            div.appendChild(label);
-            div.appendChild(field);
-
-            divAddition.appendChild(div);
-        });
-        that.editor.appendChild(divAddition);
-
-    }
-
-});
-
-
 SirTrevor.Blocks.DiwaneeImage = SirTrevor.Blocks.Image.extend({
     type: "diwanee image",
     title: function() {
@@ -126,7 +18,6 @@ SirTrevor.Blocks.DiwaneeImage = SirTrevor.Blocks.Image.extend({
     },
 
     loadData: function(data){
-        console.log(data.file.url);
         // Create our image tag
         this.editor.innerHTML = '';
         var image = document.createElement("img");
@@ -168,11 +59,20 @@ SirTrevor.Blocks.DiwaneeImage = SirTrevor.Blocks.Image.extend({
     },
 
     imageAddition: function(data){
-        console.log(data);
+
         var that = this;
-        
         var divAddition = document.createElement("div");
         divAddition.setAttribute("class", "image-additional");
+        var a = document.createElement("a");
+        a.setAttribute("data-toggle", "collapse");
+        a.setAttribute("data-target", "#image-add"+that.blockID);
+        a.innerHTML = "Image data";
+        divAddition.appendChild(a);
+
+        var divCollapsable = document.createElement("div");
+        divCollapsable.setAttribute("id", "image-add"+that.blockID);
+        divCollapsable.setAttribute("class", "collapse");
+
         $.each(this.textFields, function(i, element) {
             var div = document.createElement("div");
             div.setAttribute('class', 'image_input_text');
@@ -184,8 +84,8 @@ SirTrevor.Blocks.DiwaneeImage = SirTrevor.Blocks.Image.extend({
             if(data !== undefined) {
                 field.setAttribute("value", data[element.sel]);
             }
-             if(element.required) {
-              field.setAttribute("required", true);
+            if(element.required) {
+                field.setAttribute("required", true);
             }
             field.setAttribute("maxlength", 90);
             var label = document.createElement("label");
@@ -195,14 +95,77 @@ SirTrevor.Blocks.DiwaneeImage = SirTrevor.Blocks.Image.extend({
             div.appendChild(label);
             div.appendChild(field);
 
-            divAddition.appendChild(div);
+            divCollapsable.appendChild(div);
         });
+        divAddition.appendChild(divCollapsable);
         that.editor.appendChild(divAddition);
 
     }
 
-
 });
+
+
+SirTrevor.Blocks.SliderImage = SirTrevor.Blocks.DiwaneeImage.extend({
+
+    type: "slider image",
+    icon_name: 'iframe',        //'sliderimage',
+
+
+    loadData: function(data){
+        // Create our image tag
+        this.editor.innerHTML = '';
+
+        var div = document.createElement("div");
+        var t = document.createTextNode("Slider Image");
+        div.appendChild(t);
+        this.editor.appendChild(div);
+
+        var div = document.createElement("div");
+        var image = document.createElement("img");
+        image.setAttribute("src", data.file.url);
+        this.editor.appendChild(image);
+
+        this.imageAddition(data);
+    },
+
+    onDrop: function(transferData){
+        var file = transferData.files[0],
+            urlAPI = (typeof URL !== "undefined") ? URL : (typeof webkitURL !== "undefined") ? webkitURL : null;
+
+        // Handle one upload at a time
+        if (/image/.test(file.type)) {
+            this.loading();
+            // Show this image on here
+            this.inputs.style.display = 'none';
+            this.editor.innerHTML = '';
+
+            var div = document.createElement("div");
+            var t = document.createTextNode("Slider Image");
+            div.appendChild(t);
+            this.editor.appendChild(div);
+
+            var image = document.createElement("img");
+            image.setAttribute("src", urlAPI.createObjectURL(file));
+            this.editor.appendChild(image);
+            this.imageAddition();
+
+            this.editor.style.display = '';
+
+            this.uploader(
+                file,
+                function(data) {
+                    this.setData(data);
+                    this.ready();
+                },
+                function(error) {
+                    this.addMessage(i18n.t('blocks:image:upload_error'));
+                    this.ready();
+                }
+            );
+        }
+    }
+});
+
 
 
     SirTrevor.Blocks.Heading = SirTrevor.Blocks.Heading.extend({
