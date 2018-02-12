39a18826a1f1e19a8e766fd14690392c61f93cd6
Jelena Romcevic <jelena.romcevic@diwanee.com>
2018-02-12 13:56:57 +0100

    models

--- app/Http/Controllers/Admin/AdminNodesController.php
+++ app/Http/Controllers/Admin/AdminNodesController.php
@@ -30,7 +30,8 @@ class AdminNodesController extends Controller {
 
         $objects = [];
         if(isset($data['node_type_id'])) {
-            $objects = Node::where('node_type_id', '=', $data['node_type_id'])->get();
+            $nodeObject = new Node(['node_type_id' => $data['node_type_id']]);
+            $objects = $nodeObject::where('node_type_id', '=', $data['node_type_id'])->get();
         }
 
         return view('blocks.nodes-list', compact('objects'));
